e255ef175bf9ababb645d7f1970a750116de1718
LCCastillo03 <98895126+LCCastillo03@users.noreply.github.com>
2022-05-04 10:48:55 -0500

    porfin

--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-
 from enum import Enum
 # Importamos las librerías para la GUI
 import tkinter as tk
@@ -37,52 +36,22 @@ Casa_Estudio = tk.PhotoImage(file="casaestudio.png")
 Piso1 = tk.PhotoImage(file="piso1.png")
 Piso2 = tk.PhotoImage(file="piso2.png")
 Piso3 = tk.PhotoImage(file="piso3.png")
-
-
-class lugares (Enum):
-    CASA_ESTUDIO = "Casa estudio"
-    BIBLIOTECA = "Biblioteca"
-    BLOQUEB = "Bloque B"
-    BLOQUEC = "Bloque C"
-    BLOQUED = "Bloque D"
-    BLOQUEG = "Bloque G"
-    BLOQUEK = "Bloque K"
-
-
-class Lugar:
-    def __init__(self):
-        ubicacion: lugares
-        piso: int
-        cantidad: int
-
-    def hayDispo(): bool
-
-
-class Computador():
-    def __init__(self):
-        ID: int
-        disponibilidad: bool
-        lugar: Lugar
-
-# Esta clase valida que el usuario no ingrese letras, solo números que hacen
-# referencia  a su código estudiantil
-
-# Clase estudiante
-
-class codigos():
-    def __init__(self, text):
-        self.text = text
-
-    def validate_code(text: str):
-        return text.isdecimal()
-    
-
+Disponibles_G = tk.PhotoImage(file="disponibles.png")
 
 # PARA CREAR LAS DEMÁS VENTANAS
 # Cada función es un menú
 
 
 def MainMenu():
+    # Esta clase valida que el usuario no ingrese letras, solo números que hacen
+    # referencia  a su código estudiantil
+    class codigos():
+        def __init__(self, text):
+            self.text = text
+
+        def validate_code(text: str):
+            return text.isdecimal()
+
     # Para resetear la pantalla cuando uno se devuelva al menu principal
     for ele in ventana.winfo_children():
         ele.destroy()
@@ -103,14 +72,8 @@ def MainMenu():
     codigoEntry = Entry(ventana, textvariable=codigo, bg="#ECE7E6", width=35, validate="key",
                         validatecommand=(ventana.register(codigos.validate_code), "%S"))
     codigoEntry.place(x=473, y=385, width=100, height=30)
-    # Abrimos el archivo txt que contiene los códigos estudiantiles
 
-    filename = ("file/codigos.txt")
-    filename = ("files/codigos.txt")
-    with open(filename) as file:
-        readfile = file.read()
     # LOGIN
-
     def login():
         # Validar que el campo no este vacío
         Validar_codigo = codigoEntry.get()
@@ -125,6 +88,10 @@ def MainMenu():
                 Error = tk.Label(ventana, text="Error, código no encontrado.",
                                  font=Fuente_principal, disabledforeground=None, bg="#FFFFFF")
                 Error.place(x=600, y=385)
+    # Abrimos el archivo txt que contiene los códigos estudiantiles
+    filename = ("file/codigos.txt")
+    with open(filename) as file:
+        readfile = file.read()
 
     # boton para ingresar o acceder
     Button_login = tk.Button(ventana, text="Acceder", width=7,
@@ -132,6 +99,22 @@ def MainMenu():
     Button_login.place(x=500, y=430)
 
 # Ventana de creditos
+
+
+def Ventana_Disponibilidad_G():
+    # Destruye o resetea todo lo que se encuentra en la ventana anterior
+    for ele in ventana.winfo_children():
+        ele.destroy()
+    # Se crea la nueva Interfaz
+    interfaz = tk.Canvas(ventana)
+    interfaz.pack()
+    # En el label se carga la imagen
+    label1 = tk.Label(interfaz, image=Disponibles_G)
+    label1.pack()
+    # Boton para regresarte al menú principal y para quitarle los bordes
+    Boton_Regreso = tk.Button(ventana, text="", command=Ventana_Opciones, width=7, height=1, font=Fuente_principal,
+                              image=Imagen_Regresar, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
+    Boton_Regreso.place(x=830, y=430, width=74, height=70)
 
 
 def Ventana_creditos():
@@ -149,8 +132,27 @@ def Ventana_creditos():
                               image=Imagen_Regresar_Creditos, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
     Boton_Regreso.place(x=847, y=449, width=63, height=64)
 
+class Disponibilidad_G():
+        def __init__(self):
+            file = open("file\dispo.txt")
+            self.readfile = file.readlines()
+ 
+        def dispo_pc(self):
+            file = open("file\dispo.txt", "r")
+            readfile = file.readlines()
+            for string in readfile:
+                fila = string.split(",")
+                lugar = fila[0]
+                piso = fila[1]
+                disponibilidad = fila[2]
+                cantidad = int(fila[3])
+                if (lugar == "Bloque G"):
+                    if (disponibilidad == "true"):
+                        print(cantidad)
 
 def Ventana_Opciones():
+    dispo = Disponibilidad_G()
+    dispo.dispo_pc()
     # Destruye o resetea todo lo que se encuentra en la ventana anterior
     for ele in ventana.winfo_children():
         ele.destroy()
@@ -176,15 +178,15 @@ def Ventana_Opciones():
     Boton_PisoK = tk.Button(ventana, text="Bloque K", width=7, height=1, font=Fuente_principal,
                             image=Bloque_K, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
     Boton_PisoK.place(x=270, y=400, width=200, height=49)
-    Boton_PisoG = tk.Button(ventana, text="Bloque G", width=7, height=1, font=Fuente_principal,
-                            image=Bloque_G, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_PisoG.place(x=490, y=305, width=200, height=49)
-    Boton_Biblioteca = tk.Button(ventana, text="Biblioteca", width=7, height=1,
-                                 image=Biblioteca, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Biblioteca.place(x=490, y=400, width=200, height=49)
-    Boton_CasaE = tk.Button(ventana, text="Casa Estudio", width=7, height=1, command=Ventana_Pisos_CasaE,
-                            image=Casa_Estudio, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_CasaE.place(x=710, y=355, width=200, height=49)
+    Boton_PisoG = tk.Button(ventana, text="Bloque G", width=7, height=1, font=Fuente_principal, command= dispo.dispo_pc,
+                            image = Bloque_G, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_PisoG.place(x = 490, y = 305, width = 200, height = 49)
+    Boton_Biblioteca=tk.Button(ventana, text = "Biblioteca", width = 7, height = 1,
+                                 image = Biblioteca, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Biblioteca.place(x = 490, y = 400, width = 200, height = 49)
+    Boton_CasaE=tk.Button(ventana, text = "Casa Estudio", width = 7, height = 1, command = Ventana_Pisos_CasaE,
+                            image = Casa_Estudio, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_CasaE.place(x = 710, y = 355, width = 200, height = 49)
 
 
 def Ventana_Explicacion():
@@ -192,15 +194,15 @@ def Ventana_Explicacion():
     for ele in ventana.winfo_children():
         ele.destroy()
     # Se crea la nueva Interfaz
-    interfaz_Explicacion = tk.Canvas(ventana)
+    interfaz_Explicacion=tk.Canvas(ventana)
     interfaz_Explicacion.pack()
     # En el label se carga la imagen
-    label1 = tk.Label(interfaz_Explicacion, image=Explicacion)
+    label1=tk.Label(interfaz_Explicacion, image = Explicacion)
     label1.pack()
     # Boton para regresarte al menú principal y para quitarle los bordes
-    Boton_Regreso = tk.Button(ventana, text="", command=MainMenu, width=7, height=1, font=Fuente_principal,
-                              image=Imagen_Regresar, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Regreso.place(x=-3, y=467, width=74, height=70)
+    Boton_Regreso=tk.Button(ventana, text = "", command = MainMenu, width = 7, height = 1, font = Fuente_principal,
+                              image = Imagen_Regresar, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Regreso.place(x = -3, y = 467, width = 74, height = 70)
 
 
 def Ventana_Pisos_BloqueB():
@@ -208,21 +210,21 @@ def Ventana_Pisos_BloqueB():
     for ele in ventana.winfo_children():
         ele.destroy()
     # Se crea la nueva Interfaz
-    interfaz = tk.Canvas(ventana)
+    interfaz=tk.Canvas(ventana)
     interfaz.pack()
     # En el label se carga la imagen
-    label1 = tk.Label(interfaz, image=Fondo_Pisos)
+    label1=tk.Label(interfaz, image = Fondo_Pisos)
     label1.pack()
     # Boton para regresarte al menú principal y para quitarle los bordes
-    Boton_Regreso = tk.Button(ventana, text="", command=Ventana_Opciones, width=7, height=1, font=Fuente_principal,
-                              image=Imagen_Regresar, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Regreso.place(x=830, y=430, width=74, height=70)
-    Boton_Piso1 = tk.Button(ventana, text="",  width=7, height=1, font=Fuente_principal,
-                            image=Piso1, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Piso1.place(x=150, y=355, width=200, height=49)
-    Boton_Piso2 = tk.Button(ventana, text="",  width=7, height=1, font=Fuente_principal,
-                            image=Piso2, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Piso2.place(x=480, y=355, width=200, height=49)
+    Boton_Regreso=tk.Button(ventana, text = "", command = Ventana_Opciones, width = 7, height = 1, font = Fuente_principal,
+                              image = Imagen_Regresar, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Regreso.place(x = 830, y = 430, width = 74, height = 70)
+    Boton_Piso1=tk.Button(ventana, text = "",  width = 7, height = 1, font = Fuente_principal,
+                            image = Piso1, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Piso1.place(x = 150, y = 355, width = 200, height = 49)
+    Boton_Piso2=tk.Button(ventana, text = "",  width = 7, height = 1, font = Fuente_principal,
+                            image = Piso2, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Piso2.place(x = 480, y = 355, width = 200, height = 49)
 
 
 def Ventana_Pisos_CasaE():
@@ -230,26 +232,26 @@ def Ventana_Pisos_CasaE():
     for ele in ventana.winfo_children():
         ele.destroy()
     # Se crea la nueva Interfaz
-    interfaz = tk.Canvas(ventana)
+    interfaz=tk.Canvas(ventana)
     interfaz.pack()
     # En el label se carga la imagen
-    label1 = tk.Label(interfaz, image=Fondo_Pisos)
+    label1=tk.Label(interfaz, image = Fondo_Pisos)
     label1.pack()
     # Boton para regresarte al menú principal y para quitarle los bordes
-    Boton_Regreso = tk.Button(ventana, text="", command=Ventana_Opciones, width=7, height=1, font=Fuente_principal,
-                              image=Imagen_Regresar, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Regreso.place(x=830, y=430, width=74, height=70)
-    Boton_Piso1 = tk.Button(ventana, text="",  width=7, height=1, font=Fuente_principal,
-                            image=Piso1, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Piso1.place(x=98, y=345, width=200, height=49)
-    Boton_Piso2 = tk.Button(ventana, text="",  width=7, height=1, font=Fuente_principal,
-                            image=Piso2, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Piso2.place(x=328, y=345, width=200, height=49)
-    Boton_Piso3 = tk.Button(ventana, text="",  width=7, height=1, font=Fuente_principal,
-                            image=Piso3, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
-    Boton_Piso3.place(x=558, y=345, width=200, height=49)
-
-
+    Boton_Regreso=tk.Button(ventana, text = "", command = Ventana_Opciones, width = 7, height = 1, font = Fuente_principal,
+                              image = Imagen_Regresar, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Regreso.place(x = 830, y = 430, width = 74, height = 70)
+    Boton_Piso1=tk.Button(ventana, text = "",  width = 7, height = 1, font = Fuente_principal,
+                            image = Piso1, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Piso1.place(x = 98, y = 345, width = 200, height = 49)
+    Boton_Piso2=tk.Button(ventana, text = "",  width = 7, height = 1, font = Fuente_principal,
+                            image = Piso2, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Piso2.place(x = 328, y = 345, width = 200, height = 49)
+    Boton_Piso3=tk.Button(ventana, text = "",  width = 7, height = 1, font = Fuente_principal,
+                            image = Piso3, bg = "#FFFFFF", bd = 1, disabledforeground = None,  relief = "flat")
+    Boton_Piso3.place(x = 558, y = 345, width = 200, height = 49)
+prueba = Disponibilidad_G()
+prueba.dispo_pc()
 MainMenu()
 # El mainloop lleva el registro de todo lo que está sucediendo en la ventana:
 ventana.mainloop()
