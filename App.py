6c3c9de31dc5b7a94a0bb4b95fdd1c7d7e93a7de
mcamilagomez <blanquicettm@uninorte.edu.co>
2022-05-04 09:46:00 -0500

    4/05/2022

--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-
 from enum import Enum
 # Importamos las librerías para la GUI
 import tkinter as tk
@@ -37,52 +36,25 @@ Casa_Estudio = tk.PhotoImage(file="casaestudio.png")
 Piso1 = tk.PhotoImage(file="piso1.png")
 Piso2 = tk.PhotoImage(file="piso2.png")
 Piso3 = tk.PhotoImage(file="piso3.png")
-
-
-class lugares (Enum):
-    CASA_ESTUDIO = "Casa estudio"
-    BIBLIOTECA = "Biblioteca"
-    BLOQUEB = "Bloque B"
-    BLOQUEC = "Bloque C"
-    BLOQUED = "Bloque D"
-    BLOQUEG = "Bloque G"
-    BLOQUEK = "Bloque K"
-
-
-class Lugar:
-    def __init__(self):
-        ubicacion: lugares
-        piso: int
-        cantidad: int
-
-    def hayDispo(): bool
-
-
-class Computador():
-    def __init__(self):
-        ID: int
-        disponibilidad: bool
-        lugar: Lugar
-
-# Esta clase valida que el usuario no ingrese letras, solo números que hacen
-# referencia  a su código estudiantil
-
-# Clase estudiante
-
-class codigos():
-    def __init__(self, text):
-        self.text = text
-
-    def validate_code(text: str):
-        return text.isdecimal()
-    
-
+Disponibles_G = tk.PhotoImage(file="disponibles.png")
 
 # PARA CREAR LAS DEMÁS VENTANAS
 # Cada función es un menú
 
 
 def MainMenu():
+    # Esta clase valida que el usuario no ingrese letras, solo números que hacen
+    # referencia  a su código estudiantil
+    class codigos():
+        def __init__(self, text):
+            self.text = text
+
+        def validate_code(text: str):
+            return text.isdecimal()
+    #Disponibilidad G
+    class Disponibilidad_G():
+        def __init__(self):
+            pass
     # Para resetear la pantalla cuando uno se devuelva al menu principal
     for ele in ventana.winfo_children():
         ele.destroy()
@@ -103,14 +75,8 @@ def MainMenu():
     codigoEntry = Entry(ventana, textvariable=codigo, bg="#ECE7E6", width=35, validate="key",
                         validatecommand=(ventana.register(codigos.validate_code), "%S"))
     codigoEntry.place(x=473, y=385, width=100, height=30)
-    # Abrimos el archivo txt que contiene los códigos estudiantiles
 
-    filename = ("file/codigos.txt")
-    filename = ("files/codigos.txt")
-    with open(filename) as file:
-        readfile = file.read()
     # LOGIN
-
     def login():
         # Validar que el campo no este vacío
         Validar_codigo = codigoEntry.get()
@@ -125,6 +91,10 @@ def MainMenu():
                 Error = tk.Label(ventana, text="Error, código no encontrado.",
                                  font=Fuente_principal, disabledforeground=None, bg="#FFFFFF")
                 Error.place(x=600, y=385)
+    # Abrimos el archivo txt que contiene los códigos estudiantiles
+    filename = ("file/codigos.txt")
+    with open(filename) as file:
+        readfile = file.read()
 
     # boton para ingresar o acceder
     Button_login = tk.Button(ventana, text="Acceder", width=7,
@@ -132,6 +102,22 @@ def MainMenu():
     Button_login.place(x=500, y=430)
 
 # Ventana de creditos
+
+
+def Ventana_Disponibilidad_G():
+    # Destruye o resetea todo lo que se encuentra en la ventana anterior
+    for ele in ventana.winfo_children():
+        ele.destroy()
+    # Se crea la nueva Interfaz
+    interfaz = tk.Canvas(ventana)
+    interfaz.pack()
+    # En el label se carga la imagen
+    label1 = tk.Label(interfaz, image=Disponibles_G)
+    label1.pack()
+    # Boton para regresarte al menú principal y para quitarle los bordes
+    Boton_Regreso = tk.Button(ventana, text="", command=Ventana_Opciones, width=7, height=1, font=Fuente_principal,
+                              image=Imagen_Regresar, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
+    Boton_Regreso.place(x=830, y=430, width=74, height=70)
 
 
 def Ventana_creditos():
@@ -176,7 +162,7 @@ def Ventana_Opciones():
     Boton_PisoK = tk.Button(ventana, text="Bloque K", width=7, height=1, font=Fuente_principal,
                             image=Bloque_K, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
     Boton_PisoK.place(x=270, y=400, width=200, height=49)
-    Boton_PisoG = tk.Button(ventana, text="Bloque G", width=7, height=1, font=Fuente_principal,
+    Boton_PisoG = tk.Button(ventana, text="Bloque G", width=7, height=1, font=Fuente_principal, command=Ventana_Disponibilidad_G,
                             image=Bloque_G, bg="#FFFFFF", bd=1, disabledforeground=None,  relief="flat")
     Boton_PisoG.place(x=490, y=305, width=200, height=49)
     Boton_Biblioteca = tk.Button(ventana, text="Biblioteca", width=7, height=1,
